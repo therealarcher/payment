db9b80338158b23ff8264049b752a03ad3509e74
Jonathan Archer <jonathan.d.archer@gmail.com>
2020-01-05 17:21:07 -0500

    got basic postgres connection setup

--- src/app.js
+++ src/app.js
@@ -1,14 +1,44 @@
 const express = require("express");
-
 const app = express();
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+dotenv.config();
+
+// line below to simplify
+const queries = require("./queries");
+
 // setup port for deployment or port 3000 as local
-const port = process.env.PORT || 3000;
+const port = process.env.PORT; // || 3000
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.json());
 
-app.use(express.json());
+// PG database client/connection setup
+// const { Pool } = require("pg");
+// const dbParams = require("../lib/db.js");
+// const db = new Pool(dbParams);
+// console.log("dbParams: ", dbParams);
+// console.log(db);
+// db.connect();
 
-app.get("/", (req, res) => {
-  res.send("home page");
-});
+// app.get("/", (req, res) => {
+//   //   // res.send("home page");
+
+//   const getUsers = queries.getUsers;
+//   //   // console.log(getUsers());
+
+//   res.json({ getUsers });
+// });
+
+// *** this works ***
+app.get("/", queries.getUsers);
+
+// app.get("/", (req, res) => res.send("testing123"));
+
+// app.get("/", (req, res) => {
+//   const getUsers = queries.getUsers;
+//   res.send(getUsers());
+// });
 
 app.get("/register", (req, res) => {
   res.send("registration page");
---
+++ src/queries.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const Pool = require("pg").Pool;
+const dbParams = require("../lib/db.js");
+const db = new Pool(dbParams);
+// console.log(dbParams);
+
+const getUsers = (request, response) => {
+  db.query("SELECT * FROM users", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+// const getUsers = db
+//   .query(
+//     `
+//   SELECT * FROM users;
+// `
+//   )
+//   .then(res => {
+//     return res.rows;
+//   })
+//   .catch(err => console.error("query error", err.stack));
+
+// const getUsers = () => "testing123";
+
+module.exports = { getUsers };
